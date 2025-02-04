a5c64c820db1c49d392c2a82d47304c30322fa04
Preethi Ramakrishnan <preethidinesh20116@gmail.com>
2025-02-04 12:11:13 +0530

    newTodo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from "react";
 //import Spinwheel from "./SpinWheel/Spinwheel";
-import Todoapp from "./To-Do/Todoapp";
-
+//import Todoapp from "./To-Do/Todoapp";
+import Todo from "./To-Do/Todo";
 
 
 function App(){
 
   return(
     <div>
-     
-<Todoapp></Todoapp>
-   
-     
- 
+  <Todo></Todo>
     </div>
   );
 }
